9596a19b53a02a0fef9b0fc0451ff67d782a40ea
Yongju Lee <kyoazul@gmail.com>
2017-10-24 00:40:26 +0900

    Ch 3. 배열 : 인디언 이름 제조기 만들기

---
+++ JS/p61.js
@@ -0,0 +1,37 @@
+var colist = ["Green", "Red", "Blue", "Black"];
+var anilist = ["moose\'s", "lion\'s", "wolf\'s", "eagle\'s"];
+var namelist = ["tear", "breath", "rise", "roar"];
+
+var colors = colist[Math.floor(Math.random() * 4)];
+var animals = anilist[Math.floor(Math.random() * 4)];
+var names = namelist[Math.floor(Math.random() * 4)];
+
+var indion = colors + " " + animals + " " + names + "!!";
+console.log(indion);
+
+
+
+var colist = ["Green", "Red", "Blue", "Black"];
+var anilist = ["moose\'s", "lion\'s", "wolf\'s", "eagle\'s"];
+var namelist = ["tear", "breath", "rise", "roar"];
+
+var colors = Math.floor(Math.random() * 4);
+var animals = Math.floor(Math.random() * 4);
+var names = Math.floor(Math.random() * 4);
+
+console.log(colist[colors], anilist[animals], namelist[names] +"!!")
+
+
+
+var colist = ["Green", "Red", "Blue", "Black"];
+var anilist = ["moose\'s", "lion\'s", "wolf\'s", "eagle\'s"];
+var namelist = ["tear", "breath", "rise", "roar"];
+
+var colors = Math.floor(Math.random() * 4);
+var animals = Math.floor(Math.random() * 4);
+var names = Math.floor(Math.random() * 4);
+var first = colist[colors];
+var second = anilist[animals];
+var third = namelist[names];
+
+console.log(first,second,third + "!!");
