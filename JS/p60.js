2193f668db54c983c03aa9f57d16ead6e5690a13
Yongju Lee <kyoazul@gmail.com>
2017-10-24 00:42:52 +0900

    Ch 3. 배열 : 의사 결정기

---
+++ JS/p60.js
@@ -0,0 +1,4 @@
+var answer = ["Good idea!", "Yeah, you should try it!", "Not that a good idea.", "Maybe not today?","My computer says no."];
+var idx = Math.floor(Math.random() * 5);
+console.log("Can I have another milkshake?")
+console.log(answer[idx]);
