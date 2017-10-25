54bbaa97762c0877c45935dfca5ef69135bfb6f0
JL40 <32827095+JL40@users.noreply.github.com>
2017-10-25 17:27:57 +0900

    Delete 1025q1.js

--- 1025q1.js
+++
@@ -1,8 +0,0 @@
-var a = parseFloat(prompt("Enter a number."));
-var b = parseFloat(prompt("Enter another number."));
-
-console.log(alert("A + B" + " = " + (a + b).toFixed(2)));
-console.log(alert("A - B" + " = " + (a - b).toFixed(2)));
-console.log(alert("A x B" + " = " + (a * b).toFixed(2)));
-console.log(alert("A / B" + " = " + (a % b).toFixed(2)));
-
