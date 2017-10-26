be0327ef8ef55701c691041a937c068160605563
Yongju Lee <kyoazul@gmail.com>
2017-10-27 01:00:09 +0900

    Day 6 : 배열 - 배열의 활용 (push로 배열 만들고 pop으로 하나씩 지워나가기)

---
+++ array_7.js
@@ -0,0 +1,23 @@
+var bldng = []
+bldng.push("my house");
+bldng.push("front path");
+bldng.push("street lamp");
+bldng.push("fire hydrant");
+bldng.push("fire station");
+bldng.push("cat rescue centre");
+bldng.push("my school");
+bldng.push("Jenny\'s");
+
+console.log(bldng);
+
+//하나씩 지우기
+
+console.log(bldng.pop()); // Jenny's
+bldng.pop();
+bldng.pop();
+bldng.pop();
+bldng.pop(); 
+bldng.pop(); 
+bldng.pop(); 
+bldng.pop(); 
+console.log(bldng); // Array [ ]
