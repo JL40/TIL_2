d78d140e6d72f5d6cc96adeb8b8fc0704993260f
Yongju Lee <kyoazul@gmail.com>
2017-10-27 00:55:33 +0900

    Day 6 : 배열 - 배열의 길이를 확인하기

---
+++ array_2.js
@@ -0,0 +1,10 @@
+var Xman = ["Wolverine", "Mystique", "Beast"];
+console.log(Xman[0]); // "Wolverine"
+console.log(Xman[1]); // "Mystique"
+console.log(Xman[2]); // "Beast"
+
+console.log(Xman.length); // 배열의 길이
+console.log[Xman.length -1]; // 배열의 가장 마지막자리 원소
+
+
+
