4c9755231eb6abd5a6ec0d1638b23b5ef37b7c9c
Yongju Lee <kyoazul@gmail.com>
2017-10-27 01:08:48 +0900

    Day 6 : 배열 - 의사 결정기를 만들기

---
+++ array_9.js
@@ -0,0 +1,6 @@
+var answer = ["Good idea!", "Yeah, you should try it!", "Not that a good idea.", "Maybe not today?","My computer says no."];
+var idx = Math.floor(Math.random() * 5);
+console.log("Can I have another milkshake?");
+console.log(answer[idx]);
+console.log("Should I do my homework?");
+console.log(answer[idx]);
