a059d50be735792d9ee215d5eed76cf1bcb57cc1
Yongju Lee <kyoazul@gmail.com>
2017-10-27 00:56:47 +0900

    Day 6 :  배열 - 배열에서 원소를 제거하기

---
+++ array_4.js
@@ -0,0 +1,12 @@
+// pop
+animals = ["Polar Bear", "Monkey", "Cat", "Dog", "Lama"];
+var lastanimal = animals.pop(); 
+console.log(animals);
+animals.unshift("lastanimal");
+console.log(animals);
+
+// shift
+animals = ["Polar Bear", "Monkey", "Cat", "Dog"]
+var fstanimal = animals.shift();
+console.log(animals);
+
