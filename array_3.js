31688ea2d750b2c3bdcf6f682d8d9cfe83841a28
Yongju Lee <kyoazul@gmail.com>
2017-10-27 00:56:17 +0900

    Day 6 : 배열 - 배열에 원소를 추가하기 (push, unshift)

---
+++ array_3.js
@@ -0,0 +1,17 @@
+//push
+var animals = [];
+
+animals.push("Cat");
+animals.push("Dog");
+animals.push("Lama");
+
+console.log(animals);
+console.log(animals.length);
+
+//unshift
+var animals = ["Cat", "Dog", "Lama"];
+animals.unshift("Monkey");
+animals.unshift("Polar Bear");
+console.log(animals);
+console.log(animals[1]);
+console.log(animals[2]);
