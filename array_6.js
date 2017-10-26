3565b3261769960ef4f7ecd86316f4ef2bbc3c8d
Yongju Lee <kyoazul@gmail.com>
2017-10-27 00:58:44 +0900

    Day 6 : 배열 - 배열을 문자열로 만들기 (join)

---
+++ array_6.js
@@ -0,0 +1,17 @@
+// join
+
+var goodani = ["Monkey", "Cat", "Fish", "Gecko"];
+console.log(goodani.join());
+
+var goodani = ["Monkey", "Cat", "Fish", "Gecko"];
+console.log(goodani.join(" - "));
+console.log(goodani.join("*"));
+console.log(goodani.join(" and "));
+
+var myadd = ["Seoul", "Chongro", "Sejongro", "Gyeongbok"];
+console.log(myadd.join(" "));
+var myaddress = myadd[0] + " " + myadd[1] + " " + myadd[2] + " " + myadd[3];
+console.log(myaddress);
+
+var age = [11, 20, 33];
+age.join(" ");
