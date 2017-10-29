e1f0ba294a186dd3d8f31b96247925badf20f734
Yongju Lee <kyoazul@gmail.com>
2017-10-30 02:50:09 +0900

    Day 10 : 연습문제 - 숫자 맞추기 게임 (오타 수정)

--- numbers.js
+++ numbers.js
@@ -4,13 +4,12 @@ var count = 0;
 while(true) {
 	var number = parseInt(prompt("1에서 100까지 중 아무 숫자나 입력하세요."));    
 	if(number === answer) {
-		console.log("정답!!" + (++count) + "만에 맞췄습니다!");
+		console.log("정답!!" + (++count) + "번 만에 맞췄습니다!");
 		break;
 	} else if (number > answer) {
 		console.log((++count) + "번 틀렸습니다. 더 작은 수를 입력하세요.");
 		continue;
-		}
-    else {
+	} else {
 		console.log((++count) + "번 틀렸습니다. 더 큰 수를 입력하세요");
 	    continue;
 	}
